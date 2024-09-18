f2a54f9e828bbf5369470235d76f0dbfeec1ff06
abdullah siddiqui <Abdullah>
2024-09-18 07:57:28 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import pyautogui
+
+pyautogui.moveTo(100,100,duration=1)
+
+pyautogui.moveRel(0,50,2)
+
+pyautogui.click(290,20,duration=1)
+
+
+pyautogui.moveTo(1100,300,duration=1)
+pyautogui.scroll(569)
+pyautogui.typewrite('abdullah is foof')
